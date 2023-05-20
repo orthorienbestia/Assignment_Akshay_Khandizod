322db2d80632cd43b3d539a304cf9c915db68c3d
akshayk_mac <akshayk@github.com>
2023-05-20 12:39:12 +0530

    Food config initializer and data merged to system. Scoring system added.

---
+++ Assets/_Project/Scripts/GameScene/FoodConfigurationSystem.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace _Project.Scripts.GameScene
+{
+    public class FoodConfigurationSystem : MonoBehaviour
+    {
+        [SerializeField] private string configJsonFileName = "FoodConfigurationData";
+        [UsedImplicitly]
+        public static FoodConfigurationSystem Instance { get; private set; }
+        
+        [UsedImplicitly]
+        public List<FoodParameters> Data { get; private set; }
+        
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Addressables.LoadAssetAsync<TextAsset>(configJsonFileName).Completed += OnConfigurationFileLoaded;
+        }
+
+        private void OnConfigurationFileLoaded(AsyncOperationHandle<TextAsset> operationHandle)
+        {
+            if (operationHandle.Status == AsyncOperationStatus.Succeeded)
+            {
+                var textAsset = operationHandle.Result;
+                Data = JsonUtility.FromJson<FoodData>(textAsset.ToString()).data;
+                LogData();   
+            }
+            else
+            {
+                Debug.LogError("Failed to load JSON file: " + operationHandle.OperationException);
+            }
+        }
+        
+        private void LogData()
+        {
+            foreach (var foodParameters in Data)
+            {
+                Debug.Log($"Color: {foodParameters.Color} {foodParameters.GetColorFromHex().ToString()}, Points: {foodParameters.Points.ToString()}");
+            }
+        }
+    }
+    
+    [System.Serializable]
+    public class FoodData
+    {
+        public List<FoodParameters> data;
+    }
+
+    [System.Serializable]
+    public class FoodParameters
+    {
+        public string Color;
+        public int Points;
+
+        public Color GetColorFromHex()
+        {
+            if (ColorUtility.TryParseHtmlString(Color, out var color))
+            {
+                return color;
+            }
+
+            Debug.LogError("Invalid color string!");
+            return new Color(1, 1, 1, 1);
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/GameScene/ScoringSystem.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+namespace _Project.Scripts.GameScene
+{
+    public class ScoringSystem : MonoBehaviour
+    {
+        [SerializeField] 
+        private TMP_Text currentScoreText;
+    }
+}
